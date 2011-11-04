ce0682864525219834a3a53572e650354d332d90
Lance Gleason <lgleasain@yahoo.com>
2011-11-04 08:45:15 -0400

    changes for audio logic

--- dialplan.rb
+++ dialplan.rb
@@ -3,5 +3,10 @@ adhearsion {
 }
 
 tropo_agi {
-  play 'http://net1.madringtones.org/data/13/451546/files/451546.mp3'
+  choice = input 1, :play => 'press 1 to wait or 2 to talk to a representative'
+  if choice == 1
+    play 'http://net1.madringtones.org/data/13/451546/files/451546.mp3'
+  else
+    play 'you slacker'
+  end
 }
