2c2f67ddf2b98e1b6df0dfdba0fbb0fed15c36d5
Lance Gleason <lgleasain@yahoo.com>
2011-11-04 08:49:08 -0400

    changes for audio logic

--- dialplan.rb
+++ dialplan.rb
@@ -4,7 +4,7 @@ adhearsion {
 
 tropo_agi {
   choice = input 1, :play => 'press 1 to wait or 2 to talk to a representative'
-  if choice == 1
+  if choice == '1'
     play 'http://net1.madringtones.org/data/13/451546/files/451546.mp3'
   else
     play 'you slacker'
